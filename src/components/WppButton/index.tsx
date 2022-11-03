814fe74836b2208585b603468e34f58e924901de
aleeixoj <aleeixojunior2016@hotmail.com>
2022-11-03 11:31:32 -0300

    Ajuste no wppButton

--- src/components/WppButton/index.tsx
+++ src/components/WppButton/index.tsx
@@ -1,17 +1,23 @@
 import Link from 'next/link'
+import Router from 'next/router'
 import { FaWhatsapp } from 'react-icons/fa'
 import { Button } from '../Button'
 import styles from './styles.module.scss'
 
 function WppButton({ ...rest }) {
+  async function handleRedirect() {
+    Router.push(`${process.env.API_WPP}`)
+  }
   return (
     <div className={styles.container} {...rest}>
 
-      <a href={`${process.env.API_WPP}`} target="_blank" rel="noreferrer">
-        <Button>
-          <FaWhatsapp size={"100%"} />
-        </Button>
-      </a>
+      <Link href={`${process.env.API_WPP}`}>
+        <a target="_blank" rel="noreferrer">
+          <Button >
+            <FaWhatsapp size={"100%"} />
+          </Button>
+        </a>
+      </Link>
     </div>
   )
 }
