5df65d2e59c3c32e122954346371f6c05a68c49b
aleeixoj <aleeixojunior2016@hotmail.com>
2022-11-16 18:05:11 -0300

    Correçao de url de images

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     domains: [`localhost`, '192.168.100.42',
       'api-rentx-aleixo-s3.s3.sa-east-1.amazonaws.com', 'www.mercadopago.com',
       'img.mlstatic.com', 'www.google.com.br', 'www.jucanabalada.com.br', 'i0.wp.com', 'media.staticontent.com',
-      'fechacomdiego.herokuapp.com']
+      'fechacomdiego.herokuapp.com', 'fecha-com-diego.s3.sa-east-1.amazonaws.com']
   },
 }
 
